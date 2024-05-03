8076e0dc12853a437d0f279f08c2cc94e7814b70
Aguirrex <aguirrejuan670@gmail.com>
2024-05-03 18:39:13 -0500

    Automatizando el proceso de login a SauceDemo y a la página de la UTP

--- src/test/java/loginTest.java
+++ src/test/java/loginTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 
 public class loginTest {
 
@@ -14,7 +15,7 @@ public class loginTest {
 
     @BeforeEach
     public void setUp() {
-        String pathDriver = "";
+        String pathDriver = "/usr/bin/chromedriver/chromedriver";
         System.setProperty("webdriver.chrome.driver",pathDriver);
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized");
@@ -23,7 +24,50 @@ public class loginTest {
 
     @Test
     public void testLogin() throws InterruptedException {
-        // Thread.sleep(2000);
+        driver.get("https://www.saucedemo.com/");
+
+        WebElement usuario = driver.findElement(By.xpath("/html/body/div/div/div[2]/div[1]/div/div/form/div[1]/input"));
+
+        usuario.sendKeys("standard_user");
+
+        Thread.sleep(1000);
+
+        WebElement password = driver.findElement(By.xpath("/html/body/div/div/div[2]/div[1]/div/div/form/div[2]/input"));
+
+        password.sendKeys("secret_sauce");
+
+        Thread.sleep(1000);
+
+        WebElement boton = driver.findElement(By.xpath("/html/body/div/div/div[2]/div[1]/div/div/form/input"));
+
+        boton.click();
+
+        Thread.sleep(10000);
+
+    }
+
+    @Test
+    public void testUtpLogin() throws InterruptedException {
+
+        driver.get("https://app4.utp.edu.co/pe/");
+
+        WebElement usuario = driver.findElement(By.xpath("//*[@id=\"txtUrio\"]"));
+
+        usuario.sendKeys("1016912325");
+
+        Thread.sleep(2000);
+
+        WebElement password = driver.findElement(By.xpath("//*[@id=\"txtPsswd\"]"));
+
+        password.sendKeys("password");
+
+        Thread.sleep(2000);
+
+        WebElement boton = driver.findElement(By.xpath("/html/body/div[1]/div[5]/div[2]/div[1]/form/div[6]/button[1]/span[2]"));
+
+        boton.click();
+
+        Thread.sleep(10000);
 
     }
 
